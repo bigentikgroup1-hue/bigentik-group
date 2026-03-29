735c692d5841cbe90968025dc1eab07e9da6826b
Jerickson Abreu <Jericksonabreu@gmail.com>
2026-03-28 20:35:50 -0400

    feat: add correct data to projects

--- data/projects-i18n.ts
+++ data/projects-i18n.ts
@@ -54,6 +54,10 @@ export const projects: Project[] = [
   },
   {
     slug: "manuela-aparthotel",
+    price: "US$189,000",
+    rooms: "1/2/3",
+    units: "36",
+    locationLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.1360245329593!2d-69.5385323!3d19.319902900000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaefb506d49be39%3A0x8ad27932d88faf20!2sNicole%20Residence!5e0!3m2!1sen!2sdo!4v1774742777036!5m2!1sen!2sdo",
     title: {
       en: "Manuela Aparthotel",
       es: "Manuela Aparthotel",
@@ -61,7 +65,7 @@ export const projects: Project[] = [
     },
     description: {
       en: "Condominios en Las Terrenas con 36 apartamentos distribuidos en 5 edificios, áreas sociales y cercanía a la zona comercial.",
-      es: "Condominios en Las Terrenas con 36 apartamentos distribuidos en 5 edificios, áreas sociales y cercanía a la zona comercial.",
+      es: "El Apartotel Manuela es el sexto proyecto de condominios en Las Terrenas realizado por el Grupo de Construcción BIGENTIK. Consta de: 36 apartamentos en total, distribuidos en 5 edificios. 21 apartamentos con 2 habitaciones 7 estudios tipo Penthouse con 1 habitación 6 apartamentos con 3 habitaciones 2 estudios tipo Penthouse con 2 habitaciones Piscina, Parqueos, Área comercial de 300 m2",
       fr: "Condominios en Las Terrenas con 36 apartamentos distribuidos en 5 edificios, áreas sociales y cercanía a la zona comercial."
     },
     coverImage: "https://bigentik.com/wp-content/uploads/2024/04/Manuela-Apart-hotel-las-terrenas-grupo-bigentik-2024-map.jpg",
@@ -96,6 +100,10 @@ export const projects: Project[] = [
   },
   {
     slug: "luxury-hills",
+    price: "US$335,000",
+    rooms: "2/3",
+    units: "27",
+    locationLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.172686314608!2d-69.5577115!3d19.3183115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaefa0fcc1c45e1%3A0xfd8f9b79b6b0a742!2sBonita%20Village%20Beach%20Residence!5e0!3m2!1sen!2sdo!4v1774743029383!5m2!1sen!2sdo",
     title: {
       en: "Luxury Hills",
       es: "Luxury Hills",
@@ -103,7 +111,7 @@ export const projects: Project[] = [
     },
     description: {
       en: "Ubicado dentro de Bonita Village Residences. Cuenta con apartamentos rodeados de palmerales a pocos pasos de Playa Las Ballenas.",
-      es: "Ubicado dentro de Bonita Village Residences. Cuenta con apartamentos rodeados de palmerales a pocos pasos de Playa Las Ballenas.",
+      es: "En Luxury Hills, Bigentik Group se centró en implementar su construcción para aprovechar al máximo el espléndido paisaje circundante: colinas infinitas y palmerales. Luxury Hills está ubicado dentro de una de las comunidades más exclusivas y con una ubicación privilegiada de Las Terrenas: Bonita Village Residences. Rodeado de casas millonarias, brinda un ambiente relajante. Luxury Hills es un proyecto de 27 apartamentos en total, y a tan solo 5 minutos a pie de la magnífica playa Las Ballenas.",
       fr: "Ubicado dentro de Bonita Village Residences. Cuenta con apartamentos rodeados de palmerales a pocos pasos de Playa Las Ballenas."
     },
     coverImage: "https://bigentik.com/wp-content/uploads/2024/03/Project-Luxury-hills-Las-Terrenas-Bigentik-group-Dominican-Republic-renders-header-1024x576.jpg",
@@ -139,6 +147,10 @@ export const projects: Project[] = [
   },
   {
     slug: "sol-de-arena",
+    price: "US$329,000",
+    rooms: "2/3",
+    units: "117",
+    locationLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.999757964637!2d-69.5160428!3d19.325816799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaef104c5726bf9%3A0xb50178be9b7b638c!2sResidencial%20Sol%20de%20arena!5e0!3m2!1sen!2sdo!4v1774561821646!5m2!1sen!2sdo",
     title: {
       en: "Sol de Arena",
       es: "Sol de Arena",
@@ -146,7 +158,7 @@ export const projects: Project[] = [
     },
     description: {
       en: "Proyecto frente al mar excepcional en Playa El Portillo con un diseño ultra moderno y de clase alta.",
-      es: "Proyecto frente al mar excepcional en Playa El Portillo con un diseño ultra moderno y de clase alta.",
+      es: "Después de haber realizado muchos proyectos inmobiliarios exitosos en Las Terrenas, el GRUPO BIGENTIK ahora se muda a la exclusiva zona de la playa El Portillo, con el propósito de construir una comunidad frente al mar excepcional de 117 unidades de clase alta. Sol de Arena brinda apartamentos con un diseño ultra moderno y con grandes aperturas para disfrutar a lo máximo del paisaje de playa y de la maravillosa vista al mar. Dentro de los apartamentos, uno encuentra amplios y luminosos espacios de vida y alrededor de la impresionante piscina, áreas sociales excepcionales.",
       fr: "Proyecto frente al mar excepcional en Playa El Portillo con un diseño ultra moderno y de clase alta."
     },
     coverImage: "https://bigentik.com/wp-content/uploads/2023/05/Bigentik-group-Project-Sol-de-Arena-las-terrenas-dominican-republic.jpg",
@@ -183,6 +195,10 @@ export const projects: Project[] = [
   },
   {
     slug: "nicole-apart-hotel",
+    price: "US$290,000",
+    rooms: "2/3",
+    units: "27",
+    locationLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.1360245329593!2d-69.5385323!3d19.319902900000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaefb506d49be39%3A0x8ad27932d88faf20!2sNicole%20Residence!5e0!3m2!1sen!2sdo!4v1774742777036!5m2!1sen!2sdo",
     title: {
       en: "Nicole Apart-Hotel",
       es: "Nicole Apart-Hotel",
@@ -190,7 +206,7 @@ export const projects: Project[] = [
     },
     description: {
       en: "Iniciativa residencial de alta gama en el corazón de Las Terrenas, proporcionando apartamentos de diseño contemporáneo y amplia piscina.",
-      es: "Iniciativa residencial de alta gama en el corazón de Las Terrenas, proporcionando apartamentos de diseño contemporáneo y amplia piscina.",
+      es: "Nicole Apart-Hotel se erige como el primer gran proyecto residencial del grupo Bigentik. La visión de Nicole Apart-Hotel era proporcionar apartamentos de alta gama con diseño contemporáneo y una gran piscina, situados en el corazón de Las Terrenas. Lo que distingue a Nicole Apart-Hotel es su ubicación privilegiada, lo que permite un fácil acceso a la playa, supermercados, bancos y restaurantes. Esta característica ha convertido a Nicole Apart-Hotel en un proyecto atractivo y exitoso tanto para las familias locales como para los turistas. Comodidades: Piscina de 25 metros de largo Recepción y Administración 24/7 Gestión de alquiler Guardia de seguridad 24/7 Área Social Junto a la Piscina Planta de energía de respaldo Estacionamiento privado Lavandería",
       fr: "Iniciativa residencial de alta gama en el corazón de Las Terrenas, proporcionando apartamentos de diseño contemporáneo y amplia piscina."
     },
     coverImage: "https://bigentik.com/wp-content/uploads/2023/05/Bigentik-group-Nicole-Apart-Hotel-las-terrenas-dominican-republic.jpg",
@@ -218,6 +234,10 @@ export const projects: Project[] = [
   },
   {
     slug: "villa-loren",
+    price: "US$150,000",
+    rooms: "1/2/3/4",
+    units: "11",
+    locationLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.2017736230987!2d-69.5592274!3d19.3170488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaefbcb4b24f853%3A0xc3b2c7649641ab01!2sResidencial%20Villa%20Loren!5e0!3m2!1sen!2sdo!4v1774744005606!5m2!1sen!2sdo",
     title: {
       en: "Villa Loren",
       es: "Villa Loren",
@@ -225,7 +245,7 @@ export const projects: Project[] = [
     },
     description: {
       en: "Diseño ultra moderno con materiales de alta gama, enfoque en privacidad y tranquilidad dentro de Bonita Village.",
-      es: "Diseño ultra moderno con materiales de alta gama, enfoque en privacidad y tranquilidad dentro de Bonita Village.",
+      es: "Villa Loren es el segundo proyecto inmobiliario realizado por el Grupo Bigentik. Su ubicación dentro de las exclusivas residencias de Bonita Village inspiró un diseño ultra moderno con materiales de alta gama y un enfoque en la privacidad y la tranquilidad. El complejo se beneficia de un corto acceso a la hermosa playa de Las Ballenas. Comodidades: Piscina Jardines y paseos comunes Cancha de tenis Guardia de seguridad 24/7 Planta de energía de respaldo Estacionamiento privado",
       fr: "Diseño ultra moderno con materiales de alta gama, enfoque en privacidad y tranquilidad dentro de Bonita Village."
     },
     coverImage: "https://bigentik.com/wp-content/uploads/2023/05/VIlla-Loren-Bigentik-Group-Las-Terrenas-Dominican-Republic-6-1024x576.jpg",
@@ -251,6 +271,10 @@ export const projects: Project[] = [
   },
   {
     slug: "lorena-apart-hotel",
+    price: "US$360,000",
+    rooms: "2/3",
+    units: "6",
+    locationLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.284319373375!2d-69.5657774!3d19.313465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaef98ec2e88231%3A0xe92a543a24729210!2sEl%20Mosquito%20Boutique%20Hotel!5e0!3m2!1sen!2sdo!4v1774744179169!5m2!1sen!2sdo",
     title: {
       en: "Lorena Apart-Hotel",
       es: "Lorena Apart-Hotel",
@@ -258,7 +282,7 @@ export const projects: Project[] = [
     },
     description: {
       en: "Residencial boutique de 6 apartamentos ubicado en la magnífica zona de Playa Bonita, destacándose por sus amplios espacios.",
-      es: "Residencial boutique de 6 apartamentos ubicado en la magnífica zona de Playa Bonita, destacándose por sus amplios espacios.",
+      es: "Lorena es un proyecto residencial de solo 6 apartamentos ubicado en la magnífica zona de Playa Bonita. Diseñado para proporcionar espacios de vida ultra amplios y un acceso conveniente a la hermosa playa Bonita. Descubra el pináculo de la vida residencial en Lorena, donde la amplitud, la elegancia y el ambiente playero convergen en perfecta armonía. Comodidades: Piscina Ascensor Recepción Sala de Fitness Planta de energía de respaldo Estacionamiento privado",
       fr: "Residencial boutique de 6 apartamentos ubicado en la magnífica zona de Playa Bonita, destacándose por sus amplios espacios."
     },
     coverImage: "https://bigentik.com/wp-content/uploads/2023/05/Project-Lorena2-Las-Terrenas-Bigentik-group-Dominican-Republic--1024x576.jpg",
@@ -281,6 +305,10 @@ export const projects: Project[] = [
   },
   {
     slug: "la-palma",
+    price: "-",
+    rooms: "1/2/3/4",
+    units: "11",
+    locationLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15061.226207137304!2d-69.5683333!3d19.312499650000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaefa1c04692a7f%3A0xe78d159a90c90ba4!2sPlaya%20Bonita!5e0!3m2!1sen!2sdo!4v1774744442433!5m2!1sen!2sdo",
     title: {
       en: "La Palma",
       es: "La Palma",
@@ -288,7 +316,7 @@ export const projects: Project[] = [
     },
     description: {
       en: "Complejo de 9 elegantes apartamentos rodeado de frondosa naturaleza, cerca de la hermosa Playa Bonita, con máxima privacidad.",
-      es: "Complejo de 9 elegantes apartamentos rodeado de frondosa naturaleza, cerca de la hermosa Playa Bonita, con máxima privacidad.",
+      es: "Experimente La Palma: ¡Vida de lujo! Bienvenidos a La Palma, el último desarrollo turístico entregado por Bigentik Group. Este excepcional complejo cuenta con 9 elegantes apartamentos, rodeados de exuberantes zonas verdes y una relajante piscina. En La Palma, la prioridad es su privacidad. Con solo 2 apartamentos por edificio y sin vistas directas desde las unidades vecinas, se disfruta de un ambiente tranquilo y íntimo. Ubicada en una tranquila zona residencial, La Palma está a poca distancia a pie de la hermosa playa Bonita. Descubra la combinación perfecta de comodidad y serenidad en La Palma. Sumérjase en la belleza natural de los alrededores y relájese junto a la tentadora piscina.",
       fr: "Complejo de 9 elegantes apartamentos rodeado de frondosa naturaleza, cerca de la hermosa Playa Bonita, con máxima privacidad."
     },
     coverImage: "https://bigentik.com/wp-content/uploads/2024/01/Project-La-Palma-Las-Terrenas-Bigentik-group-Dominican-Republic-galeria-2024-6.jpg",
